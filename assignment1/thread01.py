fcd89f568db90f9c771761cf3446105d7988ce21
ballkungZ <74975135+ballkungZ@users.noreply.github.com>
2024-06-21 11:33:48 +0700

    Thread01

--- assignment1/thread01.py
+++ assignment1/thread01.py
@@ -1,1 +1,11 @@
 # running a function in another thread
+from time import sleep, ctime
+from threading import Thread
+def task():
+    sleep(1)
+    print(f'{ctime()}This os from another thread')
+
+thread = Thread(target=task)
+thread.start()
+print(f'{ctime()}Waitting for the thread')
+thread.join()
