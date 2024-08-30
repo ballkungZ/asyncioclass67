e8bebed39d7d4bdd27de3b2a7cf92874e68baaaf
ballkungZ <74975135+ballkungZ@users.noreply.github.com>
2024-08-30 09:34:38 +0700

    chess01

--- assignment07/chess01.py
+++ assignment07/chess01.py
@@ -2,7 +2,7 @@ import time
 
 my_computer_time = 0.1
 opponent_computer_time = 0.5
-opponents = 3
+opponents = 10
 move_pairs = 30
 
 def game(x):
@@ -12,11 +12,11 @@ def game(x):
         #print(f"BOARD-{x} {i+1} Judit thinking of making a move.")
         #We think for 5 seconds
         time.sleep(my_computer_time)
-        print(f"BOARD-{x} {i+1} Judit made a move.")
+        print(f"BOARD-{x+1} {i+1} Judit made a move.")
         #The opponent thinks for 5 seconds
         time.sleep(opponent_computer_time)
-        print(f"BOARD-{x} {i+1} Opponent made a move.")
-    print(f"BOARD-{x+1} - >>>>>>>>>>>>>>>>> Finished move in {round(time.perf_counter())}")
+        print(f"BOARD-{x+1} {i+1} Opponent made a move.")
+    print(f"BOARD-{x+1} - >>>>>>>>>>>>>>>>> Finished move in {round(time.perf_counter() - board_start_time)} sec")
     return round(time.perf_counter() - board_start_time)
 if __name__ == "__main__":
     start_time = time.perf_counter()
